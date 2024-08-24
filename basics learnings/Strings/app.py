aa23c6ed5db072e2e3d6848165d3f3df5b53c7a2
Bruno Mateus <brunofortunatomateus@gmail.com>
2024-08-25 01:43:35 +0200

    basics strings manipulations

---
+++ basics learnings/Strings/app.py
@@ -0,0 +1,7 @@
+course = "Python for Beginners"
+
+print(course.upper()) #PYTHON FOR BEGINNERS
+print(course.lower()) #python for beginners
+course = course.upper().replace("FOR", "4")
+print(course) #PYTHON 4 BEGINNERS
+print("4" in course) # True

11b6ceda9f07d1dfcc228e3b1736714be71dec62
Bruno Mateus <brunofortunatomateus@gmail.com>
2024-08-25 00:41:27 +0200

    casting type of datas in python

---
+++ basics learnings/Type-Conversion/app.py
@@ -0,0 +1,8 @@
+# birthday = input("Votre année de naissance: ")
+# age = 2024 - int(birthday)
+# print("Votre age est:", age)
+
+first_number = float(input("Premier Nombre: "))
+second_number = float(input("Second Nombre: "))
+sum = first_number + second_number
+print("The sum is ", sum)

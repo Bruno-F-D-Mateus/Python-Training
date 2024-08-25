1e262d8ba13848aa59dfe7e8d5c396f905c6870d
Bruno Mateus <brunofortunatomateus@gmail.com>
2024-08-25 02:08:30 +0200

    essentials operators

---
+++ basics learnings/Arithmetics-Operators/app.py
@@ -0,0 +1,12 @@
+print(10 + 3) # 13
+print(10 - 3) # 7
+print(10 * 3) # 30
+print(10 / 3) # 3.3333
+#entire division
+print(10 // 3) # 3
+print(10 % 3) # 1
+print(10 ** 2) # 100
+
+x = 10
+x += 3
+print(x) # 13

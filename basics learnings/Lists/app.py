372faf6c2b80441b2a2579b9ca763eb135b5ab57
Bruno Mateus <brunofortunatomateus@gmail.com>
2024-08-25 16:10:38 +0200

    first list interaction

---
+++ basics learnings/Lists/app.py
@@ -0,0 +1,11 @@
+names = ["Jone", "Fortunato", "Domingos", "Mateus"]
+
+names[0] = "Bruno"
+
+print(names) # ["Bruno", "Fortunato", "Domingos", "Mateus"]
+print(names[0]) # Bruno
+print(names[-1]) # Mateus   
+# -1 represents the last element in the list
+
+print(names[0:-1]) # ["Bruno", "Fortunato", "Domingos"]
+

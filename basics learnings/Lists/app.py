62fd61ea35944b522480d49bfa933fd13929593d
Bruno Mateus <brunofortunatomateus@gmail.com>
2024-09-01 16:22:44 +0200

    manipulating lists

--- basics learnings/Lists/app.py
+++ basics learnings/Lists/app.py
@@ -1,3 +1,5 @@
+"""
+
 names = ["Jone", "Fortunato", "Domingos", "Mateus"]
 
 names[0] = "Bruno"
@@ -8,4 +10,19 @@ print(names[-1]) # Mateus
 # -1 represents the last element in the list
 
 print(names[0:-1]) # ["Bruno", "Fortunato", "Domingos"]
+
+"""
+
+#Lists Méthods
+
+numbers = [1, 2, 3, 4, 5]
+
+numbers.append(6)
+print(numbers)
+
+numbers.append(1)
+print(numbers)
+
+numbers.remove(1)
+print(numbers, len(numbers))
 
